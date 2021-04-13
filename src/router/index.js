bcca0c46ee53f39a40c81a81053780dc8c54f953
Anna <bonaquista@lsc.gov>
2021-04-13 14:19:50 -0400

    404 not found page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import store from "@/store"
 
 Vue.use(VueRouter);
 
@@ -23,8 +24,25 @@ const routes = [
         component: () => import(/* webpackChunkName: "ExperienceDetails" */ "@/views/ExperienceDetails"),
         props: true,
       }
-    ]
+    ],
+    beforeEnter: (to, from, next) => {
+      const exists = store.destinations.find(
+        destination => destination.slug === to.params.slug
+      )
+      if(exists){
+        next()
+      }
+      else{
+        next({name: "notFound"})
+      }
+    }
   },
+  {
+    path: "/404",
+    alias: "*",
+    name: "notFound",
+    component: () => import(/* webpackChunkName: "NotFound" */ "@/views/NotFound"),
+  }
 ];
 
 const router = new VueRouter({
